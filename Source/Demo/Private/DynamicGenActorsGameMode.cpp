c804ba587d8fc54ff8c6546cbb9a14d7293cbeb0
renlw <renlw@glodon.com>
2023-10-14 12:52:05 +0800

    改造使用多线程读文件、多线程构造StaticMesh

--- Source/Demo/Private/DynamicGenActorsGameMode.cpp
+++ Source/Demo/Private/DynamicGenActorsGameMode.cpp
@@ -164,13 +164,137 @@ namespace
         return true;
     }
 
-    UMaterialInstanceDynamic* CreateMaterialInstanceDynamic(UMaterialInterface* SourceMaterial, float material[4])
+    void read_body_info(fstream& file, const Header_info& header, bool is_fragment, Body_info& body)
     {
-        FLinearColor Color(material[0], material[1], material[2]);
-        UMaterialInstanceDynamic* MaterialInstanceDynamic = UMaterialInstanceDynamic::Create(SourceMaterial, nullptr);
-        MaterialInstanceDynamic->SetVectorParameterValue(TEXT("BaseColor"), Color);
-        MaterialInstanceDynamic->SetScalarParameterValue(TEXT("Roughness"), material[3]);
-        return MaterialInstanceDynamic;
+        body.parentdbid = header.parentdbid;
+        body.level = header.level;
+
+        //node name
+        file.seekg(header.startname, ios::beg);
+        char* buffer = new char[header.namelength + 1];
+        file.read(buffer, header.namelength);
+        buffer[header.namelength] = '\0';
+        body.name = buffer;
+        delete[] buffer;
+
+        //node property
+        file.seekg(header.startproperty, ios::beg);
+        buffer = new char[header.propertylength + 1];
+        file.read(buffer, header.propertylength);
+        buffer[header.propertylength] = '\0';
+        body.property = buffer;
+        delete[] buffer;
+
+        file.seekg(header.startmaterial, ios::beg);
+        file.read((char*)&body.material, sizeof(body.material));
+
+        if (!is_fragment) {
+            file.seekg(header.startbox, ios::beg);
+            file.read((char*)&body.box, sizeof(body.box));
+        }
+
+        //fragment vertices
+        file.seekg(header.startvertices, ios::beg);
+
+        if (is_fragment) {
+            for (int k = 0; k < header.verticeslength / 4; k++) {
+                float f;
+                file.read((char*)&f, sizeof(f));
+                body.vertices.push_back(f);
+            }
+        }
+        else {
+            if (header.verticeslength > 0) {
+                vector<Header_info> fragment_headerList;
+                read_header_info(file, header.verticeslength / 50, &fragment_headerList);
+                int num_fragments = fragment_headerList.size();
+                body.fragment.resize(num_fragments);
+                for (int k = 0; k < num_fragments; k++) {
+                    read_body_info(file, fragment_headerList[k], true, body.fragment[k]);
+                }
+            }
+        }
+    }
+
+    bool load_file1(const std::wstring& file_name, vector<Body_info>& node_v) {
+        fstream file;
+        file.open(file_name, ios::in | ios::binary);
+        if (!file.is_open()) {
+            cout << "open the file fail" << endl;
+            return false;
+        }
+        file.seekg(0, ios::end);
+        int file_size = static_cast<int>(file.tellg());
+        cout << "file size: " << file_size << endl;
+
+        //dbid个数 = node个数
+        file.seekg(0, ios::beg);
+        int nsize;
+        file.read((char*)&nsize, sizeof(nsize));
+        cout << "node number: " << nsize << endl;
+
+        //每一个node头的大小
+        short headlength;
+        file.read((char*)&headlength, sizeof(headlength));
+        cout << "head length: " << headlength << endl;
+
+        //header
+        vector<Header_info> headerList;
+        read_header_info(file, nsize, &headerList);
+        check(headerList.size() == nsize);
+
+        int file_tellg = static_cast<int>(file.tellg());
+        cout << "file tellg: " << file_tellg << endl;
+
+        //body
+        node_v.resize(nsize);
+        for (int i = 0; i < nsize; i++) {
+            node_v[i].dbid = i;
+            read_body_info(file, headerList[i], false, node_v[i]);
+        }
+
+        file.close();
+        return true;
+    }
+
+    bool load_file2(const std::wstring& file_name, vector<Header_info>& header_list) {
+        fstream file;
+        file.open(file_name, ios::in | ios::binary);
+        if (!file.is_open()) {
+            cout << "open the file fail" << endl;
+            return false;
+        }
+
+        //dbid个数 = node个数
+        file.seekg(0, ios::beg);
+        int nsize;
+        file.read((char*)&nsize, sizeof(nsize));
+
+        //每一个node头的大小
+        short headlength;
+        file.read((char*)&headlength, sizeof(headlength));
+
+        //header
+        read_header_info(file, nsize, &header_list);
+        check(header_list.size() == nsize);
+        
+        file.close();
+        return true;
+    }
+
+    bool IsNodeValid(const Body_info& Node)
+    {
+        for (size_t j = 0; j < Node.fragment.size(); j++)
+        {
+            const Body_info& Fragment = Node.fragment[j];
+            if ((Fragment.name == "Mesh" && Fragment.vertices.size() > 9 && Fragment.vertices.size() % 9 == 0) ||
+                Fragment.name == "Elliptical" ||
+                Fragment.name == "Cylinder")
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     void AppendRawMesh(const vector<float>& vertices, TArray<FVector>& VertexList)
@@ -201,10 +325,27 @@ namespace
         //TODO:
     }
 
-    UStaticMesh* BuildStaticMesh(const TArray<FVector>& VertexList, UObject* Outer, FName Name)
+    void AppendNodeMesh(const Body_info& Node, TArray<FVector>& VertexList)
     {
-        UStaticMesh* StaticMesh = NewObject<UStaticMesh>(Outer, Name);
+        for (size_t i = 0, i_len = Node.fragment.size(); i < i_len; i++)
+        {
+            if (Node.fragment[i].name == "Mesh")
+            {
+                AppendRawMesh(Node.fragment[i].vertices, VertexList);
+            }
+            else if (Node.fragment[i].name == "Elliptical")
+            {
+                AppendEllipticalMesh(Node.fragment[i].vertices, VertexList);
+            }
+            else if (Node.fragment[i].name == "Cylinder")
+            {
+                AppendCylinderMesh(Node.fragment[i].vertices, VertexList);
+            }
+        }
+    }
 
+    void BuildStaticMesh(UStaticMesh* StaticMesh, const TArray<FVector>& VertexList)
+    {
         StaticMesh->GetStaticMaterials().Add(FStaticMaterial());//至少添加一个材质
 
         FMeshDescription MeshDesc;
@@ -225,7 +366,7 @@ namespace
             VertexInstanceIDs[i] = MeshDescBuilder.AppendInstance(VertexID);
             MeshDescBuilder.SetInstanceColor(VertexInstanceIDs[i], FVector4f(1, 1, 1, 1));
             MeshDescBuilder.SetInstanceUV(VertexInstanceIDs[i], FVector2D(0, 0));
-            MeshDescBuilder.SetInstanceNormal(VertexInstanceIDs[i], FVector(0,0,1));    //TODO:计算法线和切线
+            MeshDescBuilder.SetInstanceNormal(VertexInstanceIDs[i], FVector(0, 0, 1));    //TODO:计算法线和切线
             //MeshDescBuilder.SetInstanceTangentSpace(VertexInstanceIDs[i], FVector(), FVector(), true);
         }
 
@@ -245,7 +386,19 @@ namespace
         MeshDescPtrs.Emplace(&MeshDesc);
 
         StaticMesh->BuildFromMeshDescriptions(MeshDescPtrs, BuildParams);
+    }
 
+    void BuildStaticMesh(UStaticMesh* StaticMesh, const Body_info& Node)
+    {
+        TArray<FVector> VertexList;
+        AppendNodeMesh(Node, VertexList);
+        BuildStaticMesh(StaticMesh, VertexList);
+    }
+
+    UStaticMesh* BuildStaticMesh(const TArray<FVector>& VertexList, UObject* Outer, FName Name)
+    {
+        UStaticMesh* StaticMesh = NewObject<UStaticMesh>(Outer, Name);
+        BuildStaticMesh(StaticMesh, VertexList);
         return StaticMesh;
     }
 
@@ -264,24 +417,19 @@ namespace
     UStaticMesh* BuildStaticMesh(const Body_info& Node, UObject* Outer, FName Name)
     {
         TArray<FVector> VertexList;
-        for (size_t i = 0, i_len = Node.fragment.size(); i < i_len; i++)
-        {
-            if (Node.fragment[i].name == "Mesh")
-            {
-                AppendRawMesh(Node.fragment[i].vertices, VertexList);
-            }
-            else if (Node.fragment[i].name == "Elliptical")
-            {
-                AppendEllipticalMesh(Node.fragment[i].vertices, VertexList);
-            }
-            else if (Node.fragment[i].name == "Cylinder")
-            {
-                AppendCylinderMesh(Node.fragment[i].vertices, VertexList);
-            }
-        }
-
+        AppendNodeMesh(Node, VertexList);
         return BuildStaticMesh(VertexList, Outer, Name);
     }
+
+    UMaterialInstanceDynamic* CreateMaterialInstanceDynamic(UMaterialInterface* SourceMaterial, float material[4])
+    {
+        FLinearColor Color(material[0], material[1], material[2]);
+        UMaterialInstanceDynamic* MaterialInstanceDynamic = UMaterialInstanceDynamic::Create(SourceMaterial, nullptr);
+        MaterialInstanceDynamic->SetVectorParameterValue(TEXT("BaseColor"), Color);
+        MaterialInstanceDynamic->SetScalarParameterValue(TEXT("Roughness"), material[3]);
+        return MaterialInstanceDynamic;
+    }
+
 }
 
 void ADynamicGenActorsGameMode::BeginPlay()
@@ -291,69 +439,47 @@ void ADynamicGenActorsGameMode::BeginPlay()
     std::vector<Body_info> NodeList;
 
     FString DataFilePathName = FPaths::Combine(FPaths::ProjectDir(), TEXT("data.xsp"));
-    if (!load_file(*DataFilePathName, NodeList))
+
+#if 1
+    if (!load_file1(*DataFilePathName, NodeList))
+    {
+        FString Message = FString::Printf(TEXT("加载数据文件失败：%s"), *DataFilePathName);
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, Message, true);
+        return;
+    }
+#else
+    // 目前多线程读文件的效率并不比单线程高，多线程的主要意义应该在StaticMesh的构造，此处测试验证多线程读取文件结果正确
+    std::vector<Header_info> HeaderList;
+    if (!load_file2(*DataFilePathName, HeaderList))
     {
         FString Message = FString::Printf(TEXT("加载数据文件失败：%s"), *DataFilePathName);
         GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, Message, true);
         return;
     }
 
-    FString Message = FString::Printf(TEXT("加载数据文件完成：节点数=%d"), NodeList.size());
-    GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Message, true);
+    int32 Num = HeaderList.size();
+    NodeList.resize(Num);
+
+    auto ReadBodyFunc = [&DataFilePathName, &NodeList, &HeaderList](int32 i) {
+        fstream file;   //TODO:使用文件池
+        file.open(*DataFilePathName, ios::in | ios::binary, _SH_DENYWR);
+        Body_info& node = NodeList[i];
+        node.dbid = i;
+        read_body_info(file, HeaderList[i], false, node);
+    };
+
+    ParallelFor(Num, ReadBodyFunc, EParallelForFlags::None);
+#endif
+
+    {
+        FString Message = FString::Printf(TEXT("加载数据文件完成：节点数=%d"), NodeList.size());
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Message, true);
+    }
 
     UMaterialInterface* SourceMaterial = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), nullptr, L"/Game/DynamicGenActors/M_MainOpaque"));
     if (!SourceMaterial)
         return;
 
-#if 0
-    for (size_t i = 0; i < NodeList.size(); i++)
-    {
-        Body_info& Node = NodeList[i];
-        if (Node.fragment.size() > 0)
-        {
-            bool bHasComponent = false;
-            for (size_t j = 0; j < Node.fragment.size(); j++)
-            {
-                Body_info& Fragment = Node.fragment[j];
-                if (Fragment.name == "Mesh" && Fragment.vertices.size() > 9 && Fragment.vertices.size() % 9 == 0)
-                {
-                    bHasComponent = true;
-                    break;
-                }
-            }
-            if (!bHasComponent)
-                continue;
-
-            FString Name = FString::FromInt(Node.dbid);
-            FActorSpawnParameters ActorSpawnParameters;
-            ActorSpawnParameters.Name = *Name;
-            AActor* NodeActor = GetWorld()->SpawnActor<AActor>(ActorSpawnParameters);
-#if WITH_EDITOR
-            NodeActor->SetActorLabel(Name);
-#endif
-            {
-                USceneComponent* Component = NewObject<USceneComponent>(NodeActor, *Name);
-                NodeActor->SetRootComponent(Component);
-            }
-
-            for (size_t j = 0; j < Node.fragment.size(); j++)
-            {
-                Body_info& Fragment = Node.fragment[j];
-                if (Fragment.name == "Mesh" && Fragment.vertices.size() > 9 && Fragment.vertices.size() % 9 == 0)
-                {
-                    Name = FString::FromInt(Fragment.dbid);
-                    UStaticMeshComponent* Component = NewObject<UStaticMeshComponent>(NodeActor, *Name);
-                    UStaticMesh* StaticMesh = BuildStaticMesh(Fragment.vertices, Component, *Name);
-                    Component->SetStaticMesh(StaticMesh);
-                    Component->SetMaterial(0, CreateMaterialInstanceDynamic(SourceMaterial, Fragment.material));
-                    Component->AttachToComponent(NodeActor->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
-                }
-            }
-
-            NodeActor->RegisterAllComponents();
-        }
-    }
-#else
     FString ActorName = FPaths::GetBaseFilename(DataFilePathName);
     FActorSpawnParameters ActorSpawnParameters;
     ActorSpawnParameters.Name = *ActorName;
@@ -364,6 +490,7 @@ void ADynamicGenActorsGameMode::BeginPlay()
     USceneComponent* SceneComponent = NewObject<USceneComponent>(Actor, TEXT("RootComponent"));
     Actor->SetRootComponent(SceneComponent);
 
+#if 0
     for (size_t i = 0; i < NodeList.size(); i++)
     {
         Body_info& Node = NodeList[i];
@@ -395,7 +522,39 @@ void ADynamicGenActorsGameMode::BeginPlay()
             }
         }
     }
+#else
+    int32 NumNodes = NodeList.size();
+    TArray<UStaticMeshComponent*> StaticMeshComponentList;
+    StaticMeshComponentList.SetNumZeroed(NumNodes);
+    TArray<UStaticMesh*> StaticMeshList;
+    StaticMeshList.SetNumZeroed(NumNodes);
+    for (int32 i = 0; i < NumNodes; i++)
+    {
+        if (IsNodeValid(NodeList[i]))
+        {
+            FName ComponentName(FString::FromInt(NodeList[i].dbid));
+            StaticMeshComponentList[i] = NewObject<UStaticMeshComponent>(Actor, ComponentName);
+            StaticMeshList[i] = NewObject<UStaticMesh>(StaticMeshComponentList[i], ComponentName);
+        }
+    }
+
+    //多线程BuildStaticMesh，测试结果相对单线程耗时有明显减少，
+    auto BuildStaticMeshFunc = [&StaticMeshList, &NodeList](int32 i) {
+        if (StaticMeshList[i])
+            BuildStaticMesh(StaticMeshList[i], NodeList[i]);
+    };
+    ParallelFor(NumNodes, BuildStaticMeshFunc, EParallelForFlags::None);
+
+    for (int32 i = 0; i < NumNodes; i++)
+    {
+        if (StaticMeshComponentList[i] && StaticMeshList[i])
+        {
+            StaticMeshComponentList[i]->SetStaticMesh(StaticMeshList[i]);
+            StaticMeshComponentList[i]->SetMaterial(0, CreateMaterialInstanceDynamic(SourceMaterial, NodeList[i].material));
+            StaticMeshComponentList[i]->AttachToComponent(Actor->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+        }
+    }
+#endif
 
     Actor->RegisterAllComponents();
-#endif
 }
